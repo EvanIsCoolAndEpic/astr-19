0838cbd987622e92b80067dcaac7f8adc94f7467
Evan Egan <evansegan2025@gmail.com>
2025-10-02 12:13:03 -0700

    Create clp1.py
    That's it lol

---
+++ clp1.py
@@ -0,0 +1,6 @@
+#paste out my fav things
+#that's really it.
+print('My name: Evan')
+print('Pronouns: He/Him')
+print('My favorite movie is Hot Fuzz')
+print('My favorite food is probably... some good chinese takeout. havent found any good ones in SC yet :(')
