6c4fb66c7e945198916a735c48733305e325c243
Evan Egan <evansegan2025@gmail.com>
2025-10-14 11:10:47 -0700

    journal number3

---
+++ two-sum-num.py
@@ -0,0 +1,18 @@
+def main():
+    x = 2
+    y = 6
+    z = x + y
+    print(z, type(z))
+
+    x = 4
+    y = 3
+    z = x - y
+    print(z, type(z))
+
+    x = 2.04
+    y = 5
+    z = x * y
+    print(z, type(z))
+
+if __name__ == '__main__':
+    main()
