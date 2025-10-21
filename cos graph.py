ab73de8c70d576a680a03e1d36c6cd489a5b1153
Evan Egan <evansegan2025@gmail.com>
2025-10-21 11:52:54 -0700

    Cos graph!

---
+++ cos graph.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def main():
+    x = np.arange(0, 2 * np.pi,  0.0062831853)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
